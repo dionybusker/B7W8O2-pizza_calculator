a11d6d414fc471c18c23ee4b5e422cdba55805ad
Diony <dionybusker@gmail.com>
2021-02-12 12:06:33 +0100

    Items as buttons, w.i.p.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,43 @@
 // pizzas array can be used
 
+const pizzaList = document.getElementById("pizzaList");
+
 for (var i = 0; i < pizzas.length; i++) {
     createList(pizzas[i].name);
+    /*
+        pizzas[i].price
+        pizzas[i].img
+        pizzas[i].description
+
+        bovenstaande kunnen ook gebruikt worden in createList()
+        deze zullen dan in createList() ook nog juist aangemaakt moeten worden
+    */
 }
 
 
-// document.getElementById("pizzaList").innerHTML = txt;
 
 function createList(name) {
     var list = document.createElement("li");
-        list.innerHTML = name;
+    var btn = document.createElement("button");
+        btn.setAttribute("class", "itemBtn");
+        btn.innerHTML = name;
 
-    document.getElementById("pizzaList").appendChild(list);
+    list.appendChild(btn);
+    pizzaList.appendChild(list);
+}
+
+const itemButtons = document.querySelectorAll(".itemBtn");
+console.log(document.getElementsByClassName("itemBtn"));
+
+itemButtons.forEach(element => {
+    console.log(element)
+    // element.onclick = myAlert();
+});
+
+function myAlert() {
+    // alert(this.innerHTML)
+    console.log(this.innerHTML);
 }
 
 
-
+console.log(itemButtons)
